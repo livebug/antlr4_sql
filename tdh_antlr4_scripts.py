5a084d0d7b2f635c9d42a741ba944d515187cba5
dingxh-msi <livebug@163.com>
2025-02-21 00:32:21 +0800

    优化SQL解析器以支持子查询和改进关系提取，更新数据流图绘制功能 支持一级子查询

--- tdh_antlr4_scripts.py
+++ tdh_antlr4_scripts.py
@@ -3,9 +3,11 @@ from antlr4.InputStream import InputStream
 from tdh.antlr4.HiveLexer import HiveLexer as SqlLexer
 from tdh.antlr4.HiveParser import HiveParser as SqlParser
 from tdh.antlr4.HiveParserListener import HiveParserListener as SqlListener
+from collections import deque
 import pydot
 
 from tdh_antlr4_test import print_tree_like_linux, save_dot_file
+
 
 # 解析SQL脚本，获取表名和关联表
 def parse_sql(sql_script):
@@ -14,35 +16,85 @@ def parse_sql(sql_script):
     stream = CommonTokenStream(lexer)
     parser = SqlParser(stream)
     tree = parser.statement()
- 
+
     save_dot_file(tree, parser, "output.dot")
 
     return tree
+
 
 # 自定义监听器，提取表名和关联表
 class TableListener(SqlListener):
     def __init__(self):
         self.tables = set()
         self.relations = []
+        self.stack = deque()
+        self.subQueryStack = deque()
         self.insert = (False, None)
-    
+
     def enterInsertClause(self, ctx):
-        table_name = ctx.tableOrPartition().tableName().getText()
+        table_name = ctx.tableOrPartition().tableName().getText().lower()
         self.tables.add(table_name)
-        self.insert = (True, table_name)
+        self.stack.append(table_name)
+
+    def exitRegularBody(self, ctx):
+        if self.stack.__len__() > 0:
+            self.stack.pop()
+        return super().exitRegularBody(ctx)
 
     def enterAtomjoinSource(self, ctx):
-        table_name = ctx.getText()
-        self.tables.add(table_name)
-        if self.insert[0]:
-            self.relations.append((table_name, self.insert[1], 'insert'))
-            self.insert = (False, None)
+        if ctx.tableSource():
+            table_name = ctx.tableSource().getText().lower()
+            self.tables.add(table_name)
+            if len(self.subQueryStack) > 0:
+                right_table = self.subQueryStack[-1]
+                self.relations.append((table_name, right_table, {"label": "insert"}))
+
+        if ctx.subQuerySource():
+            table_name = "subquery#" + ctx.subQuerySource().getText().lower()
+            self.tables.add(table_name)
+            if len(self.stack) > 0:
+                self.relations.append((table_name, self.stack[-1], {"label": "insert"}))
+            self.subQueryStack.append(table_name)
+
+    def exitSubQuerySource(self, ctx):
+        if not isinstance(ctx.parentCtx, SqlParser.AtomjoinSourceContext):
+            if self.subQueryStack.__len__() > 0:
+                self.subQueryStack.pop()
+        return super().exitSubQuerySource(ctx)
+
+    def existJoinSource(self, ctx):
+        if self.subQueryStack.__len__() > 0:
+            self.subQueryStack.pop()
+        return super().existJoinSource(ctx)
 
     def enterOtherjoinSource(self, ctx):
-        right_table = ctx.parentCtx.atomjoinSource().getText()
-        left_table = ctx.joinSourcePart().getText()
-        label = ctx.joinToken().getText()
-        self.relations.append((left_table, right_table,label))
+        if len(self.subQueryStack) > 0:
+            right_table = self.subQueryStack[-1]
+        else:
+            right_table = ctx.parentCtx.atomjoinSource().getText()
+        if ctx.joinSourcePart().tableSource():
+            left_table = ctx.joinSourcePart().getText().lower()
+            label = ctx.joinToken().getText().lower()
+            self.relations.append(
+                (
+                    left_table,
+                    right_table,
+                    {"label": label, "style": "dashed", "dir": "none"},
+                )
+            )
+
+        if ctx.joinSourcePart().subQuerySource():
+            left_table = "subquery#" + ctx.joinSourcePart().getText().lower()
+            label = ctx.joinToken().getText().lower()
+            self.subQueryStack.append(left_table)
+            self.relations.append(
+                (
+                    left_table,
+                    right_table,
+                    {"label": label, "style": "dashed", "dir": "none"},
+                )
+            )
+
 
 # 获取表名和关联表
 def get_tables_and_relations(tree):
@@ -51,16 +103,27 @@ def get_tables_and_relations(tree):
     walker.walk(listener, tree)
     return listener.tables, listener.relations
 
+
 # 绘制表级数据流项图（dot）
 def draw_data_flow_graph(tables, relations, output_file):
-    graph = pydot.Dot(graph_type='digraph')
+    graph = pydot.Dot(graph_type="digraph")
     for table in tables:
         node = pydot.Node(table)
         graph.add_node(node)
-    for left_table, right_table,label in relations:
-        edge = pydot.Edge(left_table, right_table, label=label)
+    for left_table, right_table, config in relations:
+        label = config.get("label", "")
+        style = config.get("style", "solid")
+        direction = config.get("dir", "forward")
+        edge = pydot.Edge(
+            left_table,
+            right_table,
+            label=label,
+            style=style,
+            dir=direction,
+        )
         graph.add_edge(edge)
     graph.write(output_file)
+
 
 # 主函数，处理SQL脚本并绘制数据流图
 def main(sql_script, output_file):
@@ -68,12 +131,13 @@ def main(sql_script, output_file):
     tables, relations = get_tables_and_relations(tree)
     draw_data_flow_graph(tables, relations, output_file)
 
+
 if __name__ == "__main__":
     sql_script = """
     insert into table4
-    SELECT * FROM table1
-    JOIN table2 ON table1.id = table2.id
-    LEFT JOIN table3 ON table3.id = table2.cid
+    SELECT * FROM (select id from users where age > 20 ) AS table1
+    JOIN (select * from table2 ) t2 ON table1.id = t2.id
+    LEFT JOIN table3 ON table3.id = t2.cid
     """
     output_file = "data_flow_graph.dot"
     main(sql_script, output_file)
